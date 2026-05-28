985032f2483ad6151252163122925c15462f44f8
arifalsyahputra69 <arifalsyahputra69@gmail.com>
2026-05-28 17:39:48 +0700

    app.php

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://https://arifalsyahputra69.github.io/arifalsyahputra.github.io/'),
 
     /*
     |--------------------------------------------------------------------------
